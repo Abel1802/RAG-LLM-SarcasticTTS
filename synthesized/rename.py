11c2c4661de960a89a5ae5113cd69bbc6d242d08
Abel1802 <lzblcu19@gmail.com>
2025-07-06 19:22:24 +0200

    rename

---
+++ synthesized/rename.py
@@ -0,0 +1,14 @@
+import os
+import glob
+
+# 指定目录
+folder = 'vits_lora_rag'
+
+# 遍历所有 .wav 文件
+for filepath in glob.glob(os.path.join(folder, '*.wav')):
+    filename = os.path.basename(filepath)
+    if '#' in filename:
+        new_filename = filename.replace('#', '_')
+        new_filepath = os.path.join(folder, new_filename)
+        os.rename(filepath, new_filepath)
+        print(f'Renamed: {filename} → {new_filename}')
